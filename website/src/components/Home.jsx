0d06b8be37d52f683868a8ffbd9dbcd074e19a45
Danny Nothdurft <d.nothdurft@gmail.de>
2022-08-19 12:21:02 +0200

    WahtsApp Button hinzugefügt

--- website/src/components/Home.jsx
+++ website/src/components/Home.jsx
@@ -12,6 +12,11 @@ function Home() {
             <div className='Text-Container'>
                 <h1>Hi, ich bin ein <br /> <span className='Highlight'>Web-Entwickler</span></h1>
                 <p>Hi, ich bin Danny und habe gerade die Umschulung bei der <ExternalLink href="https://digitalcareerinstitute.org/">Digital Career Institute gGmbH</ExternalLink> für die Web-Entwicklung abgeschlossen.</p>
+                <br />
+                <p>Sind sie gerade auf der Suche nach einem Motivierten Mitarbeiter? <br />
+                    Dann Kontaktiere mich doch einfach.</p>
+
+                <a href="https://api.whatsapp.com/send?phone=4917656612113" title="Schreibe mir für ein Job Angebot" target="_blank" rel="noreferrer noopener"><img src="https://business-view.photo/wp-content/uploads/2020/01/whatsapp-business.png" alt="WhatsApp" className='WhatsApp-Button' /></a>
             </div>
             <div className='Image-Container'>
                 <img src={Dots} alt="35 Punkte" className='Dots-Image' />
