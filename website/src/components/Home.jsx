8c7cfca184ba768b6c09db61f3af8fdb236b3a4e
Danny Nothdurft <d.nothdurft@gmail.de>
2022-08-19 11:20:45 +0200

    Home-Componente OverflowX beseitigt

--- website/src/components/Home.jsx
+++ website/src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { ExternalLink } from 'react-external-link';
 
 import Dots from '../images/Dots_Purple.svg';
+import CirBack from '../images/Circle_Background.svg';
 import Avatar from '../images/Profil_Pic.jpeg';
 
 function Home() {
     return (
         <div className='Home-Componente'>
-            <div className='Circle-Background'></div>
+            <div className='Circle-Background'><img src={CirBack} alt="Circle Background" /></div>
             <div className='Text-Container'>
                 <h1>Hi, ich bin ein <br /> <span className='Highlight'>Web-Entwickler</span></h1>
                 <p>Hi, ich bin Danny und habe gerade die Umschulung bei der <ExternalLink href="https://digitalcareerinstitute.org/">Digital Career Institute gGmbH</ExternalLink> für die Web-Entwicklung abgeschlossen.</p>
