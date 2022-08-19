7d2c46af47481eea9d0e1d675f674371cb25235e
Danny Nothdurft <d.nothdurft@gmail.de>
2022-08-19 20:26:08 +0200

    Nachrichten Funktion

--- website/src/components/Home.jsx
+++ website/src/components/Home.jsx
@@ -12,9 +12,14 @@ function Home() {
             <div className='Text-Container'>
                 <h1>Hi, ich bin ein <br /> <span className='Highlight'>Web-Entwickler</span></h1>
                 <p>Hi, ich bin Danny und habe gerade die Umschulung bei der <ExternalLink href="https://digitalcareerinstitute.org/">Digital Career Institute gGmbH</ExternalLink> für die Web-Entwicklung abgeschlossen.</p>
-                <br />
-                <p>Sind sie gerade auf der Suche nach einem Motivierten Mitarbeiter? <br />
-                    Dann Kontaktiere mich doch einfach.</p>
+
+                <span className='Abstand'></span>
+
+                <p>Bist du gerade auf der Suche nach einem motivierten Mitarbeiter? </p>
+
+                <span className='Abstand'></span>
+
+                <p>Dann kontaktiere mich doch.</p>
 
                 <a href="https://api.whatsapp.com/send?phone=4917656612113" title="Schreibe mir für ein Job Angebot" target="_blank" rel="noreferrer noopener"><img src="https://business-view.photo/wp-content/uploads/2020/01/whatsapp-business.png" alt="WhatsApp" className='WhatsApp-Button' /></a>
             </div>
--- website/src/components/Navigation.jsx
+++ website/src/components/Navigation.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 
+import Logo from '../images/Logo.png';
+
 function Navigation() {
     return (
         <div className='Navigation-Componente'>
-            Logo
+            <NavLink to="/">
+                <img src={Logo} alt="Danny Nothdurft Logo" className='DN-Logo' />
+            </NavLink>
             <ul>
                 <li> <NavLink to="/">Home</NavLink> </li>
                 <li> <NavLink to="/aboutme">Über mich</NavLink> </li>
